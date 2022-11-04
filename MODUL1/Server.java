f6eaecfa874aed25a35200ede13df3a6a79111cf
dirgarin <107786317+dirgarin@users.noreply.github.com>
2022-11-04 16:43:34 +0700

    Add files via upload

---
+++ MODUL1/Server.java
@@ -0,0 +1,19 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        Scanner regis = new Scanner(System.in);
+        system.out.println("Selamat Datang di Restoran EAD");
+        system.out.println("Silahkan Register Terlebih Dahulu");
+        system.out.println("==================================");
+        
+        new User();
+        system.out.print("Nama: " );
+        nama = regis.nextLine();
+
+        system.out.print("No. Handphone: ");
+        nomor = regis.nextInt();
+    }
+}
---
+++ MODUL1/User.java
@@ -0,0 +1,9 @@
+public class User {
+    public static void
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
